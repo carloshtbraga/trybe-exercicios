689298180bcf5a8b9aaddcf533737a347bccf782
Carloshtbraga <carloshtbraga@gmail.com>
2022-09-28 15:07:49 -0300

    m

---
+++ fundamentos/bloco4/bloco4dia4/Funcoes.js
@@ -0,0 +1,19 @@
+function soma(a,b){
+    return a + b
+    
+}
+
+function sub(a,b){
+    return a-b
+}
+
+console.log(sub(9,8))
+
+function maiores(a,b){
+if(a > b){
+    return a + " é maior que " + b
+} else{
+    return b + " é maior que "  + a
+}
+}
+console.log(maiores(15,18))
