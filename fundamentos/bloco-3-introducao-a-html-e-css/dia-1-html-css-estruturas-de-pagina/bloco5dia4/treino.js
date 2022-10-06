1ab649e94ae4cd302f1be1e9dee9f11ce336d80b
Carloshtbraga <carloshtbraga@gmail.com>
2022-10-06 15:32:55 -0300

    oi

---
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-1-html-css-estruturas-de-pagina/bloco5dia4/treino.js
@@ -0,0 +1,12 @@
+let div = document.getElementsByClassName('divs');
+for (let index =0; index < div.length; index +=1){
+    div[index].style.border ='5px solid black';
+    div[index].style.margin= 'auto'
+    div[index].style.width = '30px'
+    div[index].style.height= '30px'
+    div[index].style.float = 'left'
+}
+
+let section1 = document.querySelector('.section1')
+section1.style.margin = '0 auto'
+
